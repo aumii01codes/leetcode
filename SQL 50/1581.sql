d48baa6b38391225246bc11e0594878211dfbbf6
Saumili Dutta <116259393+aumii01codes@users.noreply.github.com>
2024-04-03 20:55:39 +0530

    Create 1581.sql

---
+++ SQL 50/1581.sql
@@ -0,0 +1,6 @@
+#Customer Who Visited but Did Not Make Any Transactions
+
+select customer_id, count(*) as count_no_trans
+from Visits
+where visit_id not in (select visit_id from Transactions)
+group by customer_id
